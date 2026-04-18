67a32f6051ec3740359f5284f941123327fdff53
Edu-space-IDC <camixgamer222@gmail.com>
2026-04-18 09:36:17 -0500

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -201,7 +201,7 @@ export default function App() {
   // IMPORTANTE: Aquí puedes cambiar los enlaces a tus redes sociales
   const socialLinks = {
  whatsappG1: "https://chat.whatsapp.com/Lp5UmOzkj6z759tEiIov4P",
-    whatsappG2: "https://chat.whatsapp.com/FQNEF3ouyGq1dqoLA6tsfC",
+    whatsappG2: "https://chat.whatsapp.com/HIp8E0LwvSpAezR7pL0gif",
     whatsappCanal: "https://whatsapp.com/channel/0029Vb2VBhg47XeC8mnXYg3m",
     discord: "https://discord.gg/Q7m4wuQH",
     tiktok: "https://www.tiktok.com/@amigosdelmundo?_t=zm-8twlldzj4qs&_r=1",
